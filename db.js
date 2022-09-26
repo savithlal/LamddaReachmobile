564e4a690167526ee60ddb473557901a04d44399
Ayan Khan <ayan.khan@kadellabs.com>
2022-09-26 17:23:12 +0530

    Conn Timeout Error

--- db.js
+++ db.js
@@ -1,7 +1,10 @@
 var mysql = require("mysql");
 const config = require("./config.json");
 var pool = mysql.createPool({
-  connectionLimit: 10,
+  connectionLimit: 1000,
+  connectTimeout: 60 * 60 * 1000,
+  acquireTimeout: 60 * 60 * 1000,
+  timeout: 60 * 60 * 1000,
   host: config.HOST,
   user: config.USER,
   password: config.PASSWORD,
